90ee7d23d9232da884d773801b9ebb02f1a2baa3
Dan Yu Liang <dyuliang19@gmail.com>
2022-11-17 21:41:01 -0500

    Added headlines to front page (formatting issue still)

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import './App.css';
 //const API_URL = 'https://www.balldontlie.io/api/v1/players'
 
 function App() {
-    /**const [players, setPlayers] = useState([]);
-    const [searchVal, setSearchVal] = useState("");**/
 
-    /** const searchPlayers = async (id) => {
-        const response = await fetch(`${API_URL}/${id}`);
-        const data = await response.json();
-
-        setPlayers(data.Search)
-    } **/
     const [news, setNews] = useState([]);
-    const getNews = () => {
+    const GetNews = () => {
         fetch(`https://tabm1jhbeb.execute-api.us-west-2.amazonaws.com/Alpha`)
             .then((response) => response.json())
             .then((responseJson) => {
-                setNews(responseJson);
+                const parsed = responseJson;
+                let hlines1 = `${parsed[0]["title"]}`
+                let hlines2 = `${parsed[1]["title"]}`
+                let hlines3 = `${parsed[2]["title"]}`
+                let hlines4 = `${parsed[3]["title"]}`
+                let hlines5 = `${parsed[4]["title"]}`
+
+                let headlines = `${hlines1} ${hlines2} ${hlines3} ${hlines4} ${hlines5}`;
+
+                //let headlines = `${parsed[0]["title"]} ${parsed[1]["title"]} \n${parsed[2]["title"]} ${parsed[3]["title"]} ${parsed[4]["title"]}`
+                setNews(headlines);
             })
             .catch((error) => {
                 console.error(error);
@@ -25,14 +27,16 @@ function App() {
     };
 
     useEffect(() => {
-        getNews();
+        GetNews();
     }, []);
 
     return (
-        <div className="app">
-            <h1> NBA News</h1>
-            <useEffect></useEffect>
+        <div className="App">
+            <h1> Today's Headlines: </h1>
+            <GetNews></GetNews> 
             {JSON.stringify(news, null, 2)}
+
+            <h1> Standings: </h1>
         </div>
     );
 }
