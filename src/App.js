cf042c2ee981bea1280813b1109fb42da603a051
Dan Yu Liang <dyuliang19@gmail.com>
2022-11-20 19:37:45 -0500

    Added section for Western standings and today's scores

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import './App.css';
 function App() {
 
     const [news, setNews] = useState([]);
-    const [standings, setStandings] = useState([]);
+    const [eastStandings, setEastStandings] = useState([]);
+    const [westStandings, setWestStandings] = useState([]);
+    const [gamesToday, setGamesToday] = useState([]);
+
     const GetNews = () => {
         fetch(`https://tabm1jhbeb.execute-api.us-west-2.amazonaws.com/Alpha`)
             .then((response) => response.json())
             .then((responseJson) => {
                 setNews(responseJson);
-                console.log(responseJson);
                 return responseJson.news;
             })
             .catch((error) => {
@@ -19,7 +21,7 @@ function App() {
             });
     };
 
-    const GetStandings = () => {
+    const GetEastStandings = () => {
         const options = {
             method: 'GET',
             headers: {
@@ -31,7 +33,42 @@ function App() {
         fetch('https://api-nba-v1.p.rapidapi.com/standings?league=standard&season=2022&conference=east', options)
             .then(res => res.json())
             .then(res => {
-                setStandings(res.response);
+                console.log(res.response)
+                setEastStandings(res.response);
+            })
+            .catch(err => console.error(err));
+    };
+
+    const GetWestStandings = () => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '1756b7c53cmshd87a2f2cd4e125ep1eec65jsn5b1acc0a1e61',
+                'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+            }
+        };
+
+        fetch('https://api-nba-v1.p.rapidapi.com/standings?league=standard&season=2022&conference=west', options)
+            .then(res => res.json())
+            .then(res => {
+                setWestStandings(res.response);
+            })
+            .catch(err => console.error(err));
+    };
+
+    const GetGamesToday = () => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '1756b7c53cmshd87a2f2cd4e125ep1eec65jsn5b1acc0a1e61',
+                'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+            }
+        };
+
+        fetch('https://api-nba-v1.p.rapidapi.com/games?date=2022-11-20', options)
+            .then(res => res.json())
+            .then(res => {
+                setGamesToday(res.response);
                 console.log(res.response)
             })
             .catch(err => console.error(err));
@@ -40,7 +77,9 @@ function App() {
 
     useEffect(() => {
         GetNews();
-        GetStandings();
+        GetEastStandings();
+        GetWestStandings();
+        GetGamesToday();
     }, []);
 
     return (
@@ -55,10 +94,20 @@ function App() {
                 ))}
             </ul>
             <br></br>
-            <h1> Standings: </h1>
+            <h1> Standings </h1>
+            <h2> Eastern Conference: </h2>
             <ul>
-                <h2>Atlantic Division</h2>
-                {standings.map(output => <div>{output.team.name} (W-L): {output.win.total}-{output.loss.total}</div>)}
+                {eastStandings.map(output => <div>{output.team.name} (W-L): {output.win.total}-{output.loss.total}</div>)}
+            </ul>
+
+            <h2> Western Conference: </h2>
+            <ul>
+                {westStandings.map(output => <div>{output.team.name} (W-L): {output.win.total}-{output.loss.total}</div>)}
+            </ul>
+
+            <h1> Today's Games </h1>
+            <ul>
+                {gamesToday.map(output => <div>{output.teams.home.name} {output.scores.home.points}  -  {output.teams.visitors.name} {output.scores.visitors.points}</div>)}
             </ul>
 
         </div>
