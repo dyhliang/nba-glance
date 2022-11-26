f0bf7ae5fa1187bd7fe0b2139ea2edebf7063f7a
Dan Yu Liang <dyuliang19@gmail.com>
2022-11-26 17:35:27 -0500

    Added additional info to whpf page

--- src/components/WhpfPage.js
+++ src/components/WhpfPage.js
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 let teamName = "";
+let playerName = "";
 
 function WhpfPage() {
     const [rndPlayer, setRndPlayer] = useState([]);
     const [guess, setGuess] = useState("");
     const [feedback, setFeedback] = useState("");
+    const [moreInfo, setMoreInfo] = useState([]);
+    const [isHidden, setIsHidden] = useState(false);
 
     const getRndPlayer = () => {
         let randomNum = (Math.floor(Math.random() * (493 - 1 + 1)) + 1).toString();
@@ -15,10 +18,32 @@ function WhpfPage() {
             .then(res => {
                 setRndPlayer(res);
                 teamName = res["team"]["full_name"];
-                console.log(res);
+                playerName = res["last_name"].toLowerCase();
+                let some_link = `https://api-nba-v1.p.rapidapi.com/players?search=${playerName}`;
+                lookupPlayer(some_link);
+                console.log(res, playerName, some_link);
             })
             .catch(err => console.error(err));
+
     };
+
+    const lookupPlayer = (playerUrl) => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '1756b7c53cmshd87a2f2cd4e125ep1eec65jsn5b1acc0a1e61',
+                'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+            }
+        };
+
+        fetch(playerUrl, options)
+            .then(res => res.json())
+            .then(res => {
+                setMoreInfo(res.response);
+                console.log(res.response);
+            })
+            .catch(err => console.error(err));
+    }
 
     const checkGuess = () => {
         if (guess === teamName) {
@@ -27,6 +52,10 @@ function WhpfPage() {
             setFeedback(`Wrong - He last played for the ${teamName}.`);
         }
     };
+
+    const getMoreInfo = event => {
+        setIsHidden(current => !current);
+    }
 
     function refreshPage() {
         window.location.reload(false);
@@ -44,9 +73,8 @@ function WhpfPage() {
             <h1> Who He Play For? </h1>
 
             <br></br>
-            <ul>
-                <div>{rndPlayer.first_name} {rndPlayer.last_name} - {rndPlayer.position} </div>
-            </ul>
+
+            <h2>{rndPlayer.first_name} {rndPlayer.last_name} - ({rndPlayer.position}) </h2>
 
             <br></br>
             <form onSubmit={(event) => {
@@ -64,9 +92,21 @@ function WhpfPage() {
                 <br></br>
                 <br></br>
                 {feedback}
+                <br></br>
             </form>
+
+            <div><button onClick={getMoreInfo}> More info </button></div>
+            {isHidden &&
+                (
+                    <div> 
+                        <div>Height: {rndPlayer.height_feet} feet {rndPlayer.height_inches} inches </div>
+                        <div>College: {moreInfo.map(output => output.affiliation)} </div>
+                        <div>Debut: {moreInfo.map(output => output.nba.start)}</div>
+                    </div>
+                )
+            }
+
             <button onClick={refreshPage}> Another Player </button>
-            
 
             <footer>
                 <br></br>
