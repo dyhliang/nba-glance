fb4175e2db46efa756b9f8417c6cfea32f3a75ec
Yu Liang <liangy3@oregonstate.edu>
2023-02-24 13:31:24 -0500

    WIP: Added field of 30 teams for dropdown menu in WHPF

--- src/components/WhpfPage.js
+++ src/components/WhpfPage.js
@@ -19,6 +19,7 @@ function WhpfPage() {
         fetch(`https://www.balldontlie.io/api/v1/players/${randomNum}`)
             .then(res => res.json())
             .then(res => {
+                console.log(res);
                 setRndPlayer(res);
                 teamName = res["team"]["full_name"];
                 playerFirstName = res["first_name"];
@@ -34,7 +35,6 @@ function WhpfPage() {
         fetch(playerUrl, options)
             .then(res => res.json())
             .then(res => {
-                console.log(res);
                 let searchResults = res.response.map(output => output);
                 for (let i = 0; i < searchResults.length; i++) {
                     if (searchResults[i]["firstname"] === playerFirstName) {
@@ -75,17 +75,43 @@ function WhpfPage() {
             <form onSubmit={(event) => {
                 event.preventDefault();
             }}
-            >
-                <input
-                    type='text'
-                    id='user-guess'
-                    placeholder="Enter: City Team Name"
-                    value={guess}
+            >   Guess:
+                <select name="team-names" id="user-guess">
+                    <option value="ATL">Atlanta Hawks</option>
+                    <option value="BOS">Boston Celtics</option>
+                    <option value="BKN">Brooklyn Nets</option>
+                    <option value="CHA">Charlotte Hornets</option>
+                    <option value="CHI">Chicago Bulls</option>
+                    <option value="CLE">Cleveland Cavaliers</option>
+                    <option value="DAL">Dallas Mavericks</option>
+                    <option value="DEN">Denver Nuggets</option>
+                    <option value="DET">Detroit Pistons</option>
+                    <option value="GSW">Golden State Warriors</option>
+                    <option value="HOU">Houston Rockets</option>
+                    <option value="IND">Indiana Pacers</option>
+                    <option value="LAC">LA Clippers</option>
+                    <option value="LAL">Los Angeles Lakers</option>
+                    <option value="MEM">Memphis Grizzlies</option>
+                    <option value="MIA">Miami Heat</option>
+                    <option value="MIL">Milwaukee Bucks</option>
+                    <option value="MIN">Minnesota Timberwolves</option>
+                    <option value="NOP">New Orleans Pelicans</option>
+                    <option value="NYK">New York Knicks</option>
+                    <option value="OKC">Oklahoma City Thunder</option>
+                    <option value="ORL">Orlando Magic</option>
+                    <option value="PHI">Philadelphia 76ers</option>
+                    <option value="PHX">Phoenix Suns</option>
+                    <option value="POR">Portland Trail Blazers</option>
+                    <option value="SAC">Sacramento Kings</option>
+                    <option value="SAS">San Antonio Spurs</option>
+                    <option value="TOR">Toronto Raptors</option>
+                    <option value="UTA">Utah Jazz</option>
+                    <option value="WAS">Washington Wizards</option>
                     onChange={(e) => setGuess(e.target.value)}
-                />
-                &nbsp; &nbsp;
+                </select>
+                &nbsp; &nbsp; &nbsp;
                 <button onClick={checkGuess}> Guess! </button>
-                &nbsp; &nbsp;
+                &nbsp; &nbsp; &nbsp;
                 <button onClick={refreshPage}> Another Player </button>
                 <br></br>
                 <br></br>
